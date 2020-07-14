b4d8701787250f0fd084bdaeab1c2536c274349c
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-07-14 11:09:45 +0530

    XAMARIN-36141 - Sample and screenshot added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/ViewModel/ContactsViewModel.cs
@@ -0,0 +1,215 @@
+﻿using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class ContactsViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+
+        public ObservableCollection<Contacts> contactsinfo { get; set; }
+        public Command<object> ShowPopup { get; set; }
+        public Command<object> ClosePopup { get; set; }
+
+        private Contacts selectedItem;
+        public Contacts SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                if (selectedItem != value)
+                {
+                    selectedItem = value;
+                    this.OnPropertyChanged("SelectedItem");
+                }
+            }
+        }
+        #endregion
+
+        #region Constructor
+
+        public ContactsViewModel()
+        {
+            ShowPopup = new Command<object>(ShowPopupPage);
+            ClosePopup = new Command<object>(ClosePopupPage);
+            contactsinfo = new ObservableCollection<Contacts>();
+            GenerateInfo();
+        }
+        #endregion
+
+        private async void ShowPopupPage(object sender)
+        {
+            //Setting binding context for the popup page to show the tapped item.
+            var popupPage = new ListViewPage();
+            popupPage.BindingContext = this;
+            await PopupNavigation.Instance.PushAsync(popupPage);
+        }
+
+        private async void ClosePopupPage(object sender)
+        {
+            //Set selected item as null to clear the selection.
+            SelectedItem = null;
+            await PopupNavigation.Instance.PopAsync(true);
+        }
+
+        public void GenerateInfo()
+        {
+            Random r = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                var contact = new Contacts(CustomerNames[i], r.Next(720, 799).ToString() + " - " + r.Next(3010, 3999).ToString());
+                contact.ContactImage = ImageSource.FromResource("ListViewXamarin.Images.Image" + r.Next(0, 28) + ".png");
+                contactsinfo.Add(contact);
+            }
+        }
+
+        #region Fields
+
+        string[] CustomerNames = new string[] {
+            "Kyle",
+            "Gina",
+            "Irene",
+            "Katie",
+            "Jack",
+            "Liz",
+            "Zeke",
+            "Noah",
+            "Pete",
+            "Bill",
+            "Daniel",
+            "Frank",
+            "Brenda",
+            "Danielle",
+            "Fiona",
+            "Howard",
+            "Jack",
+            "Larry",
+            "Holly",
+            "Jennifer",
+            "Liz",
+            "Pete",
+            "Steve",
+            "Vince",
+            "Zeke",
+            "Aiden",
+            "Jackson",
+            "Mason",
+            "Liam",
+            "Jacob",
+            "Jayden",
+            "Ethan",
+            "Noah",
+            "Lucas",
+            "Logan",
+            "Caleb",
+            "Caden",
+            "Jack",
+            "Ryan",
+            "Connor",
+            "Michael",
+            "Elijah",
+            "Brayden",
+            "Benjamin",
+            "Nicholas",
+            "Alexander",
+            "William",
+            "Matthew",
+            "James",
+            "Landon",
+            "Nathan",
+            "Dylan",
+            "Evan",
+            "Luke",
+            "Andrew",
+            "Gabriel",
+            "Gavin",
+            "Joshua",
+            "Owen",
+            "Daniel",
+            "Carter",
+            "Tyler",
+            "Cameron",
+            "Christian",
+            "Wyatt",
+            "Henry",
+            "Eli",
+            "Joseph",
+            "Max",
+            "Isaac",
+            "Samuel",
+            "Anthony",
+            "Grayson",
+            "Zachary",
+            "David",
+            "Christopher",
+            "John",
+            "Isaiah",
+            "Levi",
+            "Jonathan",
+            "Oliver",
+            "Chase",
+            "Cooper" ,
+            "Tristan",
+            "Colton",
+            "Austin",
+            "Colin",
+            "Charlie",
+            "Dominic",
+            "Parker",
+            "Hunter",
+            "Thomas",
+            "Alex",
+            "Ian",
+            "Jordan",
+            "Cole",
+            "Julian",
+            "Aaron",
+            "Carson",
+            "Miles",
+            "Blake",
+            "Brody",
+            "Adam",
+            "Sebastian",
+            "Adrian",
+            "Nolan",
+            "Sean",
+            "Riley",
+            "Bentley",
+            "Xavier",
+            "Hayden",
+            "Jeremiah",
+            "Jason",
+            "Jake",
+            "Asher",
+            "Micah",
+            "Jace",
+            "Brandon",
+            "Josiah",
+            "Hudson",
+            "Nathaniel",
+            "Bryson",
+            "Ryder",
+            "Justin",
+            "Bryce",
+        };
+        #endregion
+
+        #region Interface Member
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+}
---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Views/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Rg.Plugins.Popup.Extensions;
+using Rg.Plugins.Popup.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ListViewXamarin
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MainPage : ContentPage
+	{
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
